7ef1cdeea43391cc781870fdd0f99a7394e59040
ali1k <hyperir@gmail.com>
2015-05-11 09:58:57 +0200

    encoded URIs

--- rdf_convertor.js
+++ rdf_convertor.js
@@ -16,23 +16,23 @@ rdf.setBuiltins();
 module.exports = {
   convert: function(record){
     var g= new Graph;
-    var uri= 'wosR:'+record['qname'];
+    var uri= 'wosR:' + encodeURIComponent(record['qname']);
     //rdf:type
     if(record['DT']){
         g.add(env.createTriple(uri, rdfns('type'), 'wosV:'+record['DT'][0].replace(' ','_')));
     }
     if(record['UT']){
-        g.add(env.createTriple(uri, 'owl:sameAs', 'urn:'+record['UT'][0]));
+        g.add(env.createTriple(uri, 'owl:sameAs', 'urn:'+encodeURIComponent(record['UT'][0])));
     }
     if(record['DI']){
-        g.add(env.createTriple(uri, 'owl:sameAs', 'doi:'+record['DI'][0]));
+        g.add(env.createTriple(uri, 'owl:sameAs', 'doi:'+encodeURIComponent(record['DI'][0])));
     }
     if(record['CR']){
       _.forEach(record['CR'], function(citation) {
-        g.add(env.createTriple(uri, 'wosV:CR', 'wosR:'+citation['qname']));
-        g.add(env.createTriple('wosR:'+citation['qname'], 'rdfs:label', citation['citation'].l()));
+        g.add(env.createTriple(uri, 'wosV:CR', 'wosR:'+encodeURIComponent(citation['qname'])));
+        g.add(env.createTriple('wosR:'+encodeURIComponent(citation['qname']), 'rdfs:label', citation['citation'].l()));
         if(citation['doi']){
-          g.add(env.createTriple('wosR:'+citation['qname'], 'owl:sameAs', 'doi:'+citation['doi']));
+          g.add(env.createTriple('wosR:'+citation['qname'], 'owl:sameAs', 'doi:'+encodeURIComponent(citation['doi'])));
         }
       });
     }
@@ -41,11 +41,11 @@ module.exports = {
       //do not disambiguate authors, not sure if it is good to have unique authors?!!
       _.forEach(record['AF'], function(author,k) {
         author_id=author.replace(/\./g,'').replace(/,\s/g,' ').replace(/\s/g,'_');
-        g.add(env.createTriple(uri, 'wosV:AF', 'wosR:'+author_id));
-        g.add(env.createTriple('wosR:'+author_id, 'rdfs:label', author.l()));
+        g.add(env.createTriple(uri, 'wosV:AF', 'wosR:'+encodeURIComponent(author_id)));
+        g.add(env.createTriple('wosR:'+encodeURIComponent(author_id), 'rdfs:label', author.l()));
         //add skos lat label for short author names
         if(record['AU'] && record['AU'][k]){
-          g.add(env.createTriple('wosR:'+author_id, 'skos:altLabel', record['AU'][k].l()));
+          g.add(env.createTriple('wosR:'+encodeURIComponent(author_id), 'skos:altLabel', record['AU'][k].l()));
         }
       });
     }
